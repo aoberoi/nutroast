96fab8acb13026dfbd35ecdf961649f362ad165a
Ankur Oberoi <aoberoi@gmail.com>
2012-09-08 17:17:44 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nutroast::Application.config.secret_token = 'ac75d1c604c6269c381f0c2c6693c728f11f58b3d6a209c19428d2cab800985c169fcea7c35fea47309954fd503faee18d937546fb232306c5c1732042353d64'
