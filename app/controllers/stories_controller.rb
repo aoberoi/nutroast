a6bc23e7ecef53f94be72817e27dbdeee6d5d034
Koushik Dasika <kdasika@koushikdasika.com>
2012-09-09 12:00:12 -0400

    editted the new story html and controller action plus create

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,10 +2,22 @@ class StoriesController < ApplicationController
   before_filter :authenticate_user!
 
   def new
+    @roast = Roast.find params[:roast_id]
     @story = Story.new
   end
 
   def create
+    @roast = Roast.find params[:roast_id]
+    @story = Story.create params[:story]
+    respond_to do |format|
+      if @story.save
+        format.html { redirect_to roast_path(@roast), :notice => 'Story have been created.' }
+        format.json { render :json => @roast, :status => :created, :location => @roast }
+      else
+      	format.html { render :action => "new" }
+        format.json { render :json => @story.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def show
