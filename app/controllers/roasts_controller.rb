444435706675832591ef444488ff3d0e1ac1330f
Koushik Dasika <kdasika@koushikdasika.com>
2012-09-08 21:52:25 -0400

    finished the create new roasts page

--- app/controllers/roasts_controller.rb
+++ app/controllers/roasts_controller.rb
@@ -1,8 +1,23 @@
 class RoastsController < ApplicationController
 	def show
+    @roast = Roast.find(params[:id])
 	end
 
   def new
     @roast = Roast.new
-  end  
+  end
+  
+  def create
+    @roast = Roast.create(params[:roast])
+    respond_to do |format|
+      if @roast.save
+     
+      	format.html { redirect_to @roast, :notice => 'Roast has been created.' }
+        format.json { render :json => @roast, :status => :created, :location => @roast }
+      else
+      	format.html { render :action => "new" }
+        format.json { render :json => @roast.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
